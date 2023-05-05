eaf78b80da4c1d2c12ba61dc13805fe64fce3efa
MathiasParionaM12 <130614521+MathiasParionaM12@users.noreply.github.com>
2023-05-05 10:00:32 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
+from flask import (
+    Flask, 
+    )
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/'
+db = SQLAlchemy(app)
+
+#Models
+
+
 
