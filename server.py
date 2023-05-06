2da7dc3b9a7c039af75e5d5a8a947717730f9e6e
MathiasParionaM12 <130614521+MathiasParionaM12@users.noreply.github.com>
2023-05-06 17:04:07 -0500

    class Usuario

--- server.py
+++ server.py
@@ -11,6 +11,32 @@ db = SQLAlchemy(app)
 
 #Models
 
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.String(36), nullable=False, default=lambda: str(uuid.uuid4()), primary_key=True)
+    nombre = db.Column(db.String(), nullable=False)
+    apellido = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    contraseña = db.Column(db.String(), nullable=False)
+    foto = db.Column(db.String(), nullable=True)
+    fecha_nacimiento = db.Column(db.Date(), nullable=True)
+    biografia = db.Column(db.String(), nullable=True)
+    intereses = db.Column(db.String(), nullable=True)
+
+    def __init__(self, nombre, apellido, email, contraseña, foto=None, fecha_nacimiento=None, biografia=None, intereses=None):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.contraseña = contraseña
+        self.foto = foto
+        self.fecha_nacimiento = fecha_nacimiento
+        self.biografia = biografia
+        self.intereses = intereses
+
+    def __repr__(self):
+        return f"Usuario(id={self.id}, nombre={self.nombre}, apellido={self.apellido})"
+
+
 class Cursos(db.Model):
     __tablename__ = 'Curso'
     id = db.Column(db.String(7), nullable=False, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
@@ -27,9 +53,19 @@ class Cursos(db.Model):
         self.foto = foto
         self.cupos_disponibles = cupos_disponibles
 
+    def __repr__(self):
+        return f"Cursos(id={self.id}, nombre={self.nombre}, precio={self.precio})"
+
+
+#routes
+
 
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
 else:
     print('Importing {}'.format(__name__))    
+    
+    
+
+
