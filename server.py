f62636c7a1e323b83151256b453e3530ed3e085f
MathiasParionaM12 <mathias.pariona@utec.edu.pe>
2023-05-17 01:40:15 -0500

    actulizacion

--- server.py
+++ server.py
@@ -1,108 +1,213 @@
 from flask import (
     Flask,
     render_template, 
-    redirect, 
-    url_for
+    request, 
     )
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import session, flash, redirect, url_for
 import uuid;
 from datetime import datetime
+from sqlalchemy import LargeBinary
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/educaweb1'
-db = SQLAlchemy(app)
+server = Flask(__name__)
+server.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hp:1234@localhost:5432/educaweb1'
+db = SQLAlchemy(server)
 
 #Models
 
 from sqlalchemy import func
 
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'
-    id = db.Column(db.String(36), nullable=False, default=lambda: str(uuid.uuid4()), primary_key=True)
-    nombre = db.Column(db.String(), nullable=False)
-    apellido = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), nullable=False)
-    contrase単a = db.Column(db.String(), nullable=False)
-    foto = db.Column(db.String(), nullable=True)
-    fecha_nacimiento = db.Column(db.Date(), nullable=True)
-    biografia = db.Column(db.String(), nullable=True)
-    intereses = db.Column(db.String(), nullable=True)
-    created_at = db.Column(db.DateTime, default=func.now())
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    apellido = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable = False)
+    celular = db.Column(db.Integer, nullable = True)
+    contrasena = db.Column(db.String(80), nullable=False)
+    foto = db.Column(LargeBinary, nullable=True)
+    universidad = db.Column(db.String(50), nullable = False)
+    ciclo  = db.Column(db.Integer, nullable = True)
+    carrera = db.Column(db.String(80), nullable = True)
+    fecha_nacimiento = db.Column(db.Date(), nullable=False)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, nombre, apellido, email, contrase単a, foto=None, fecha_nacimiento=None, biografia=None, intereses=None):
+    def __init__(self, nombre, apellido, universidad, fecha_nacimiento, email,contrasena, celular=None, 
+                 foto=None, ciclo=None,carrera=None):
         self.nombre = nombre
         self.apellido = apellido
         self.email = email
-        self.contrase単a = contrase単a
+        self.celular = celular
+        self.contrasena = contrasena
         self.foto = foto
+        self.universidad = universidad
+        self.ciclo = ciclo
+        self.carrera = carrera
         self.fecha_nacimiento = fecha_nacimiento
-        self.biografia = biografia
-        self.intereses = intereses
-
-    def __repr__(self):
-        return f"Usuario(id={self.id}, nombre={self.nombre}, apellido={self.apellido})"
 
 
-class Cursos(db.Model):
-    __tablename__ = 'Curso'
-    id = db.Column(db.String(7), primary_key=True)
-    nombre = db.Column(db.String(30), nullable=False)
-    precio = db.Column(db.Float, nullable=False)
-    descripcion = db.Column(db.String(500), nullable=False)
-    foto = db.Column(db.String(500), nullable=True)
-    cupos_disponibles = db.Column(db.Integer, nullable=False)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(60), nullable=False)
+    descripcion = db.Column(db.Text, nullable=False)
+    foto = db.Column(LargeBinary, nullable=False)
+    
 
 
-    def __init__(self, nombre, precio, descripcion, foto, cupos_disponibles):
-        self.id = str(uuid.uuid4())
+    def __init__(self, nombre, precio, descripcion, foto):
         self.nombre = nombre
-        self.precio = precio
         self.descripcion = descripcion
         self.foto = foto
-        self.cupos_disponibles = cupos_disponibles
-
-    def __repr__(self):
-        return f"Cursos(id={self.id}, nombre={self.nombre}, precio={self.precio})"
+      
+class Controlador(db.Model):
+    __tablename__ = 'controladores'
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.utcnow)
+    def __init__(self,fecha_creacion):
+        self.fecha_creacion = fecha_creacion
 
     
-class Premium(Usuario):
-    __tablename__ = 'premium'
-    __mapper_args__ = {
-        'polymorphic_identity': 'miembro_pay',
-    }
-    def __init__(self, name, lastname, nickname, fecha_de_nacimiento, codeforces_handle, atcoder_handle, vjudge_handle):
-        super().__init__(name, lastname, nickname, fecha_de_nacimiento, codeforces_handle, atcoder_handle, vjudge_handle)
 
-class Freemium(Usuario):
-    __tablename__ = 'freemium'
-    __mapper_args__ = {
-        'polymorphic_identity': 'miembro_free',
-    }
-    def __init__(self, name, lastname, nickname, fecha_de_nacimiento, codeforces_handle, atcoder_handle, vjudge_handle):
-        super().__init__(name, lastname, nickname, fecha_de_nacimiento, codeforces_handle, atcoder_handle, vjudge_handle)
+    
+class Premiuns(db.Model):
+    __tablename__ = "Premiun"
+    id = db.Column(db.String(7), primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+    contrasena = db.Column(db.String(), nullable=False)
+    tarjeta_credito = db.Column(db.String())
+    fecha_caducidad = db.Column(db.DateTime, default=datetime.utcnow)
+    numseguridad = db.Column(db.String())
+    tiempodesuscripcion = db.Column(db.Integer)
 
-with app.app_context():
+    def __init__(self, id, nombre, contrasena, tarjeta_credito, fecha_caducidad, numseguridad, tiempodesuscripcion):
+        self.id = id
+        self.nombre = nombre
+        self.contrasena = contrasena
+        self.tarjeta_credito = tarjeta_credito
+        self.fecha_caducidad = fecha_caducidad
+        self.numseguridad = numseguridad
+        self.tiempodesuscripcion = tiempodesuscripcion
+
+with server.app_context():
     db.create_all()
 
 #routes
-@app.route('/', methods=['GET'])
+@server.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET'])
+@server.route('/log', methods=['GET'])
+def log():
+    return render_template('login.html')
+
+@server.route('/cursos', methods=['GET'])
+def cursos():
+    return render_template('cursos.html')
+
+@server.route('/mate', methods=['GET'])
+def mate():
+    return render_template('mate.html')
+
+@server.route('/comu', methods=['GET'])
+def comu():
+    return render_template('comu.html')
+
+@server.route('/progra', methods=['GET'])
+def progra():
+    return render_template('progra.html')
+
+@server.route('/planes', methods=['GET'])
+def planes():
+    return render_template('planes.html')
+
+@server.route('/premiun', methods=['GET'])
+def premiun():
+    return render_template('premiun.html')
+
+@server.route('/perfil')
+def perfil():
+    if 'user_id' not in session:
+        flash('Debes iniciar sesión para acceder a esta página.')
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
+
+    usuario = Student.query.get(user_id)
+
+    if not usuario:
+        flash('El usuario no existe.')
+        return redirect(url_for('login'))
+
+    return render_template('perfil.html', usuario=usuario)
+
+@server.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        contrasena = request.form['contrasena']
+        usuario = Student.query.filter_by(email=email).first()
+
+        if usuario and usuario.contrasena == contrasena:
+            session['user_id'] = usuario.id
+            flash('¡Inicio de sesión exitoso!')
+
+            return redirect(url_for('profile'))
+
+        #flash('Credenciales inválidas. Por favor, inténtalo de nuevo.')
+    
     return render_template('login.html')
 
 
+@server.route('/crear-cuenta', methods=['GET', 'POST'])
+def crear_cuenta():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        celular = request.form['celular']
+        contrasena = request.form['contrasena'] 
+        universidad = request.form['universidad']
+        ciclo = request.form['ciclo']
+        carrera = request.form['carrera']
+        fecha_nacimiento = datetime.strptime(request.form['fecha_nacimiento'], '%Y-%m-%d').date()
+
+        usuario = Student(nombre=nombre, apellido=apellido, email=email, celular=celular, 
+                          contrasena=contrasena, universidad=universidad,
+                          ciclo=ciclo, carrera=carrera, fecha_nacimiento=fecha_nacimiento)
+        
+        db.session.add(usuario)
+        db.session.commit()
+        #flash('¡Cuenta creada exitosamente! Por favor, inicia sesión.')
+        return redirect(url_for('login'))
+
+    return render_template('crear_cuenta.html')
+
+@server.route('/premiun', methods=['POST'])
+def premiun1():
+    nombre = request.form.get('nombre')
+    contrasena = request.form.get('contrasena')
+    tarjeta = request.form.get('tarjeta')
+    numseguridad = request.form.get('numseguridad')
+    caducidad = request.form.get('caducidad')
+    tiempo = request.form.get('tiempo')
+
+    # Crear una instancia de la clase Premiun con los datos del formulario
+    premiun = Premiuns(id=str(uuid.uuid4())[:7],nombre=nombre,contrasena=contrasena,tarjeta_credito=tarjeta,fecha_caducidad=caducidad,numseguridad=numseguridad,tiempodesuscripcion=int(tiempo)
+    )
+
+    # Guardar el objeto en la base de datos
+    db.session.add(premiun)
+    db.session.commit()
+    db.session.close()
+    
+    return redirect('/cursos')  # Redirigir al usuario a cursos.html
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    server.run(debug=True)
 else:
     print('Importing {}'.format(__name__))    
     
     
-
 
