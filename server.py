2e16d368015c7a4cf0ab089f81a501193a14cacf
unknown <gian.arteaga@utec.edu.pe>
2023-05-16 23:22:15 -0500

    aaa

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 import uuid;
 from datetime import datetime
 from sqlalchemy import LargeBinary
-
+from flask import session, flash, redirect, url_for
 server = Flask(__name__)
 server.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/educaweb1'
 db = SQLAlchemy(server)
@@ -149,11 +149,41 @@ with server.app_context():
 def index():
     return render_template('index.html')
 
-@server.route('/login', methods=['GET'])
+@server.route('/perfil')
+def perfil():
+    if 'user_id' not in session:
+        flash('Debes iniciar sesión para acceder a esta página.')
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
+
+    usuario = Student.query.get(user_id)
+
+    if not usuario:
+        flash('El usuario no existe.')
+        return redirect(url_for('login'))
+
+    return render_template('perfil.html', usuario=usuario)
+
+
+@server.route('/login', methods=['GET', 'POST'])
 def login():
-    usuarios_registrado = Student.query.filter()
+    if request.method == 'POST':
+        email = request.form['email']
+        contrasena = request.form['contrasena']
+        usuario = Student.query.filter_by(email=email).first()
+
+        if usuario and usuario.contrasena == contrasena:
+            session['user_id'] = usuario.id
+            flash('¡Inicio de sesión exitoso!')
+
+            return redirect(url_for('profile'))
+
+        #flash('Credenciales inválidas. Por favor, inténtalo de nuevo.')
+    
     return render_template('login.html')
 
+     
 @server.route('/crear-cuenta', methods=['GET', 'POST'])
 def crear_cuenta():
     if request.method == 'POST':
@@ -161,8 +191,7 @@ def crear_cuenta():
         apellido = request.form['apellido']
         email = request.form['email']
         celular = request.form['celular']
-        contrasena = request.form['contrasena']  # Updated argument name
-        #foto = request.form['foto']
+        contrasena = request.form['contrasena'] 
         universidad = request.form['universidad']
         ciclo = request.form['ciclo']
         carrera = request.form['carrera']
@@ -174,6 +203,9 @@ def crear_cuenta():
         
         db.session.add(usuario)
         db.session.commit()
+        #flash('¡Cuenta creada exitosamente! Por favor, inicia sesión.')
+        return redirect(url_for('login'))
+
     return render_template('crear_cuenta.html')
 
 
@@ -185,7 +217,4 @@ if __name__ == '__main__':
     server.run(debug=True)
 else:
     print('Importing {}'.format(__name__))    
-    
-    
-
 
